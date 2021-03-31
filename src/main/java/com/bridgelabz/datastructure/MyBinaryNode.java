99d8f1a599a073ff764dc148554148c012aef252
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-31 23:33:56 +0530

    [Prajakta]: Created a BST by first add 56 and then addd left side 30 and right 70 and passed the getsize

---
+++ src/main/java/com/bridgelabz/datastructure/MyBinaryNode.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.datastructure;
+
+public class MyBinaryNode<K extends Comparable<K>>
+{
+    public K key;
+    MyBinaryNode<K> left;
+    MyBinaryNode<K> right;
+
+    public MyBinaryNode(K key)
+    {
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+}
